b28fc6aec04bfb4c5b39f9697aee3c7a26ed3a4e
stormil <vasyelen@mail.ru>
2018-04-13 22:03:39 +0300

    first tasks

---
+++ 1/for/3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Guess
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Random random = new Random();
+            int randomNumber = random.Next(1, 147);
+            Console.WriteLine("Угадайте цифру от 1 до 147");
+            int userNumber;
+            do
+            {
+                userNumber = Convert.ToInt32(Console.ReadLine());
+                if (userNumber > randomNumber)
+                {
+                    Console.WriteLine("меньше");
+                }
+                else if (userNumber < randomNumber)
+                {
+                    Console.WriteLine("больше");
+                }
+                else
+                {
+                    Console.WriteLine("Поздравляем! Вы угадали слово!");
+                }
+            }
+            while (userNumber != randomNumber);
+            Console.ReadKey();
+        }
+    }
+}
