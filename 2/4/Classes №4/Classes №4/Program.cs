d27e191bb04c8d327cacd421a8bbe5bf99453154
stormil <vasyelen@mail.ru>
2018-04-17 20:14:38 +0300

    fixed 2.4

--- 2/4/Classes №4/Classes №4/Program.cs
+++ 2/4/Classes №4/Classes №4/Program.cs
@@ -12,10 +12,8 @@ namespace Classes__4
             Point point1 = new Point("A",1,2);
             Point point2 = new Point("B", -1, 8);
             Point point3 = new Point("C", 3, -5);
-            Point point4 = new Point("D", 3, -5);
-            Point point5 = new Point("E", 3, -5);
 
-            Figure figure = new Figure(point1, point2, point3, point4, point5);
+            Figure figure = new Figure(point1, point2, point3);
 
             figure.PerimeterCalculator();
             Console.ReadKey();
