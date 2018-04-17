e8a1b3fc19f2eaaa6b491b27df4a1e6ac626f5b4
stormil <vasyelen@mail.ru>
2018-04-17 13:21:17 +0300

    2.3 task

---
+++ 2/3/3/Author.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Author
+    {
+        private string writer;
+
+        public Author(string writer)
+        {
+            this.writer = writer;
+        }
+
+        public string Writer { get => writer; set => writer = value; }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Author: " + writer);
+        }
+    }
+}
---
+++ 2/3/3/Content.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Content
+    {
+        private string contentOfSomething;
+
+        public Content(string contentOfSomething)
+        {
+            this.contentOfSomething = contentOfSomething;
+        }
+
+        public string ContentOfSomething { get => contentOfSomething; set => contentOfSomething = value; }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Content:" + contentOfSomething);
+        }
+    }
+}
