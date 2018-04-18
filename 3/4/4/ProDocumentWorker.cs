c35b6fdc721080f8991a8da745b2be439554061c
stormil <vasyelen@mail.ru>
2018-04-18 17:52:38 +0300

    3.4 done

---
+++ 3/4/4/ProDocumentWorker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class ProDocumentWorker : DocumentWorker
+    {
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ отредактирован");
+        }
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+        }
+    }
+}
---
+++ 3/4/4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string key;
+            Console.WriteLine("Введите ключ доступа:");
+            key = Console.ReadLine();
+
+            DocumentWorker documentWorker;
+            if (key=="exp")
+            {
+                documentWorker = new ExpertDocumentWorker();
+            }
+            else if (key == "pro")
+            {
+                documentWorker = new ProDocumentWorker();
+            }
+            else
+            {
+                documentWorker = new DocumentWorker();
+            }
+
+            Console.WriteLine("Что вы хотите сделать с докуметом?");
+            Console.WriteLine("1 - открыть \n2 - отредактировать \n3 - сохранить");
+
+            int action = Convert.ToInt32(Console.ReadLine());
+            switch (action)
+            {
+                case 1:
+                    documentWorker.OpenDocument();
+                    break;
+                case 2:
+                    documentWorker.EditDocument();
+                    break;
+                case 3:
+                    documentWorker.SaveDocument();
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
