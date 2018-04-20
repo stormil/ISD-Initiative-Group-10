8959ba6dbc22fd5341cf5d15e7b7c0ed4f0216b0
stormil <vasyelen@mail.ru>
2018-04-20 21:44:24 +0300

    remove project (reverted from commit 183867b39b697f6ee680884ee9ac97e8c7409c04)

---
+++ 3/4/4/ExpertDocumentWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
+        }
+    }
+}
