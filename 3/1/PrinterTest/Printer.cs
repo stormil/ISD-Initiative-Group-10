581f64018739c9f91804037d75f30b1753437b4f
IvanCodeC <33967592+IvanCodeC@users.noreply.github.com>
2018-04-20 09:05:00 +0300

    done

---
+++ 3/1/PrinterTest/Printer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinterTest
+{
+    class Printer
+    {
+        public void Print(string value)
+        {
+            if(typeof(Printer) == GetType())
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            Console.WriteLine(value);
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ 3/1/PrinterTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer prin = new Printer();
+
+            prin.Print("askjfhl");
+
+            OtherPrinter opr = (OtherPrinter)prin;
+
+            opr.Print("lasjhgfdjksg");
+
+            Console.ReadKey();
+
+        }
+    }
+}
