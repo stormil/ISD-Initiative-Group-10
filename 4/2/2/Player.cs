8a5d975d152b27c5fd11a56dfa5a60e5a9366094
stormil <vasyelen@mail.ru>
2018-04-22 15:15:57 +0300

    4.2 add

---
+++ 4/2/2/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Player: IPlayable, IRecodable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing music");
+        }
+
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Pause music");
+        }
+
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Stop music");
+        }
+
+        public void Record()
+        {
+            Console.WriteLine("Recording");
+        }
+
+        void IRecodable.Pause()
+        {
+            Console.WriteLine("Pause recording");
+        }
+
+        void IRecodable.Stop()
+        {
+            Console.WriteLine("Stop recording");
+        }
+    }
+}
---
+++ 4/2/2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPlayable player = new Player();
+            player.Play();
+            IRecodable recorder = (IRecodable)player;
+            recorder.Record();
+            Console.ReadKey();
+        }
+    }
+}
