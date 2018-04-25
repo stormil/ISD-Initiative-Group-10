691529722741496f358474cd707e24c853dd09a2
stormil <vasyelen@mail.ru>
2018-04-25 12:26:10 +0300

    task 5.2 done

---
+++ 5/2/2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input size of array:");
+            if (int.TryParse(Console.ReadLine(), out int n))
+            {
+                int[] a = new int[n];
+                Console.WriteLine("Input array:");
+                for (int i = 0; i < n; i++)
+                {
+                    if (!int.TryParse(Console.ReadLine(), out a[i]))
+                    {
+                        Console.WriteLine("Wrong input");
+                        i--;
+                    }
+                }
+                a.SortQuick(0,n-1);
+
+                Console.WriteLine("Sorted array:");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(a[i] + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong input");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5/2/2/Sorter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    static class Sorter
+    {
+        static public int Partition(int[] array, int left, int right)
+        {
+            int pivot = array[left];
+            while (true)
+            {
+                while (array[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (array[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {  
+                    int temp = array[right];
+                    array[right] = array[left];
+                    array[left] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
+        static public void SortQuick(this int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(array, left, right);
+                if (pivot > 1)
+                {
+                    SortQuick(array, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    SortQuick(array, pivot + 1, right);
+                }
+            }
+        }
+    }
+}
