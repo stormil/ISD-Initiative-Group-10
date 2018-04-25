32c2778e43d67e45010609502cb117a9c239bbde
IvanCodeC <otb.tatotake@gmail.com>
2018-04-25 16:56:50 +0300

    Task 1 done

---
+++ 5/1/CalculatorTest/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTest
+{
+    static class Calculator
+    {
+        public static int Plus(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public static int Minus(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public static int Multiplication(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public static int Division(int firstNumber, int secondNumber)
+        {
+            try
+            {
+                return firstNumber / secondNumber;
+            }
+            catch(Exception e)
+            {
+                Console.Write("Division on 0: ");
+                return -1;
+            }
+        }
+    }
+}
---
+++ 5/1/CalculatorTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CalculatorTest.Calculator;
+using static System.Console;
+
+namespace CalculatorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"2 + 2 = {Plus(2,2)}");
+            WriteLine($"2 - 2 = {Minus(2, 2)}");
+            WriteLine($"2 * 2 = {Multiplication(2, 2)}");
+            WriteLine($"2 / 2 = {Division(2, 2)}");
+            WriteLine($"2 / 0 = {Division(2, 0)}");
+
+            ReadKey();
+        }
+    }
+}
