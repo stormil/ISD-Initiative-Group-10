757e898a1a413d2bd86cb89151e76e34021950eb
borysSudos <37542319+borysSudos@users.noreply.github.com>
2018-04-25 23:59:04 +0300

    5.4 Borys

---
+++ 5/5.4/5.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "s, string";
+            string k = "";
+            s.Replace(k);
+
+            string p = "2.2, string";
+            string q = "";
+            p.IndexOf(q) ;
+
+            int g = 2;
+            int l = 0;
+            g.SubString(l);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5/5.4/5.4/Util.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5._4
+{
+    static class Util
+    {
+        public static void Replace(this string a, string b)
+        {
+            Console.WriteLine("REPLACED!!!");
+
+        }
+
+        public static void IndexOf( this string value )
+        {
+            Console.WriteLine("IndexOf!!");
+        }
+
+        public static void SubString (this int i, int j)
+        {
+            Console.WriteLine(5);
+        }
+    }
+}
+
