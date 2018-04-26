4de0cc927d68caac62e4903bd8a380c8ffe50c21
olgseey <matyushchenkk@outlook.com>
2018-04-26 17:45:51 +0300

    Added 3 project

---
+++ 5/3/static3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input size of array:");
+            if (int.TryParse(Console.ReadLine(), out int n))
+            {
+                int[] a = new int[n];
+                Console.WriteLine("Input array:");
+                for (int i = 0; i < n; i++)
+                {
+                    if (!int.TryParse(Console.ReadLine(), out a[i]))
+                    {
+                        Console.WriteLine("Wrong input");
+                        i--;
+                    }
+                }
+                
+                a.SortQuick(0, n - 1);
+
+                Console.WriteLine("Input 1 if you want sorting from the beginning to the end");
+                Console.WriteLine("Else input no 1");
+                int flag = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sorted array:");
+                if (flag == 1)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.Write(a[i] + " ");
+                    }
+                }
+                else
+                {
+                    for(int i = n - 1; i >= 0; i--)
+                    {
+                        Console.Write(a[i] + " ");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong input");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5/3/static3/Sorter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static3
+{
+    static class Sorter
+    {
+        public static void SortQuick(this int[] a, int left, int right)
+        {
+            if (left > right || left < 0 || right < 0) return;
+
+            int index = Partition(a, left, right);
+
+            if (index != -1)
+            {
+                SortQuick(a, left, index - 1);
+                SortQuick(a, index + 1, right);
+            }
+        }
+
+        private static int Partition(int[] A, int left, int right)
+        {
+            if (left > right) return -1;
+
+            int end = left;
+
+            int pivot = A[right];
+            for (int i = left; i < right; i++)
+            {
+                if (A[i] < pivot)
+                {
+                    Swap(A, i, end);
+                    end++;
+                }
+            }
+
+            Swap(A, end, right);
+
+            return end;
+        }
+
+        private static void Swap(int[] A, int left, int right)
+        {
+            int tmp = A[left];
+            A[left] = A[right];
+            A[right] = tmp;
+        }
+    }
+}
