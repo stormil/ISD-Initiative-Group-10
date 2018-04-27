b6c3bcb64af39962742bb3adb6c4d112efc489a1
BorysSudos <boruha18@gmail.com>
2018-04-27 23:52:11 +0300

    Borys

---
+++ 6/6.4/6.4/CountToBirthday.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._4
+{
+    class CountToBirthday
+    {
+        int years;
+        DateTime Age;
+        DateTime birthday;
+        public CountToBirthday (string birthday)
+        {
+            DateTime tempDate;
+            if (DateTime.TryParse(birthday, out tempDate))
+                Birthday = tempDate;
+
+            Birthday.Year += (DateTime.Today.Year - Birthday.Year);
+            if (DateTime.Today.Month < Birthday.Month || (DateTime.Today.Month == Birthday.Month && DateTime.Today.Day < Birthday.Day))
+                years--;         
+        }
+
+        public int Years { get => years; set => years = value; }
+        public DateTime Birthday
+        {
+            get
+            {
+                return this.Birthday.Year;
+            }
+            set
+            {
+                this.birthday = new DateTime(value, this.Birthday.Month, this.Birthday.Date);
+            }
+        }
+
+    public override string ToString()
+        {
+            return "Моя дата рождения " + Birthday;
+        }        
+    }
+}
---
+++ 6/6.4/6.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите свою дату рождения (гггг.мм.дд) " );
+            string birthday = Console.ReadLine();
+            CountToBirthday guy = new CountToBirthday(birthday);
+            Console.WriteLine(guy.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
