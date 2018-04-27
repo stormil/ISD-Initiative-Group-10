6430b76c39518d5677eeacde1445435cd23c3215
olgseey <matyushchenkk@outlook.com>
2018-04-27 20:23:32 +0300

    made 2nd task

---
+++ 6/2/6.3/6.3/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._3
+{
+    struct Train
+    {
+        public string NameOfDestination;
+        public int Number;
+        public DateTime Date;
+    }
+    class ArrayOfTrains
+    {
+        private Train[] array = new Train[8];
+        private static int SizeOfArray = 8;
+       public void Initialisation()
+        {
+            for(int i = 0; i < SizeOfArray; i++)
+            {
+                Console.WriteLine("Input number of Train: ");
+                array[i].Number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input destination: ");
+                array[i].NameOfDestination = Console.ReadLine();
+                Console.WriteLine("Input Year, Month and Day of departure: ");
+                array[i].Date = new DateTime(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+            }
+            sorting();
+        }
+
+        public void sorting()
+        {
+            Train temp;
+            for (int i = 0; i < SizeOfArray - 1; i++)
+            {
+                for (int j = i + 1; j < SizeOfArray; j++)
+                {
+                    if (array[i].Number > array[j].Number)
+                    {
+                        temp.Date = array[i].Date;
+                        temp.Number = array[i].Number;
+                        temp.NameOfDestination = array[i].NameOfDestination;
+
+                        array[i].Date = array[j].Date;
+                        array[i].Number = array[j].Number;
+                        array[i].NameOfDestination = array[j].NameOfDestination;
+
+                        array[j].Date = temp.Date;
+                        array[j].Number = temp.Number;
+                        array[j].NameOfDestination = temp.NameOfDestination;
+                    }
+                }
+            }
+        }
+        public void InfoTrain(int number)
+        {
+            int counter = -1;
+            for(int i = 0; i < SizeOfArray; i++)
+            {
+                if(array[i].Number == number)
+                {
+                    counter = i;
+                }
+            }
+            if(counter < 0)
+            {
+                Console.WriteLine("There are trains with your number not-exists");
+            }
+            else
+            {
+                Console.Write("Number of Train: ");
+                Console.Write(array[counter].Number + "\n");
+                Console.Write("Destination: ");
+                Console.Write(array[counter].NameOfDestination + "\n");
+                Console.Write("Year, Month and Day of departure: ");
+                Console.Write(array[counter].Date + "\n");
+            }
+        }
+    }
+}
---
+++ 6/2/6.3/6.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayOfTrains trains = new ArrayOfTrains();
+            trains.Initialisation();
+            Console.WriteLine("Input Number of searching train: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            trains.InfoTrain(number);
+            Console.ReadKey();
+        }
+    }
+}
