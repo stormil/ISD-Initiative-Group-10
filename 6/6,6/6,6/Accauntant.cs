4ba3dff04cb781f18c7baf7b1c527f91fa74c8aa
BorysSudos <boruha18@gmail.com>
2018-04-28 22:24:43 +0300

    Borys Sudos commit

---
+++ 6/6,6/6,6/Accauntant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_6
+{
+    enum Post : int {  CEO=8,  secretary=10, cleaner=12 }
+
+    class Accauntant
+    {              
+        public bool AskForBonus (Post worker, int hours)
+        {
+            if (hours>(int)worker)
+            {
+                Console.WriteLine("Вы получите премию ");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Вы не получите премию");
+                return false;
+            }
+        }
+    }
+}
---
+++ 6/6,6/6,6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accauntant accauntant = new Accauntant();
+            accauntant.AskForBonus(Post.CEO, hours:2);
+            Console.WriteLine("Директор поработал и получит зарплату");
+            accauntant.AskForBonus(Post.CEO, hours: 10);
+            Console.WriteLine("Директор молодец хорошо поработал");
+            accauntant.AskForBonus(Post.secretary, hours: 2);
+            Console.WriteLine("Секретарь поработал и получит свою зарплату");
+            accauntant.AskForBonus(Post.secretary, hours: 12);
+            Console.WriteLine("Секретарь молодец хорошо поработал");
+            accauntant.AskForBonus(Post.cleaner, hours: 3);
+            Console.WriteLine("Уборщик поработал и получит свою зарплату");
+            accauntant.AskForBonus(Post.cleaner, hours: 13);
+            Console.WriteLine("Уборщик молодец хорошо поработал");
+            Console.ReadKey();
+        }
+    }
+}
