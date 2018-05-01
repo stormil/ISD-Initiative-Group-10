ab34710d873dad6039ab6c0bd16e5ea3980dcc12
stormil <vasyelen@mail.ru>
2018-05-01 09:31:01 +0300

    task 8.3 done

---
+++ 8/3/3/MyDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class MyDictionary<TKey, TValue>
+    {
+        Dictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
+        public int Amount { get => dictionary.Count; }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return dictionary[key];
+            }
+            set
+            {
+                dictionary[key] = value;
+            }
+        }
+        public void Add(TKey key, TValue value)
+        {
+            dictionary[key] = value;
+        }
+    }
+}
---
+++ 8/3/3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new MyDictionary<int, string>();
+            dictionary[23] = "work horse";
+            dictionary.Add(1, "day 2");
+
+            Console.WriteLine(dictionary[23]);
+            Console.WriteLine(dictionary[1]);
+            Console.WriteLine("Amount of elements: " + dictionary.Amount);
+            Console.ReadKey();
+        }
+    }
+}
