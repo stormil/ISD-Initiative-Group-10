26d1c4db32a91c8fcb36e5ba39378677ca7739f9
Borys Sudos <boris.sudos@gmail.com>
2018-05-01 23:45:51 +0200

    8.1 Borys done

---
+++ 8/1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class MyFactory<T> where T : new()
+    {
+        public static T FacrotyMethod()
+        {
+            return new T();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFactory<float> myFactory1 = new MyFactory<float>
+            {
+                 
+            };
+            MyFactory<int> myFactory2 = new MyFactory<int>
+            {
+
+            };
+            MyFactory<double> myFactory3 = new MyFactory<double>
+            {
+
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
