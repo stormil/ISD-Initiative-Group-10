94cafd0e67244448ce785987c7adaf2d4cda23a8
olgseey <matyushchenkk@outlook.com>
2018-05-02 17:53:59 +0300

    added 2nd task

---
+++ 8/2/2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            MyList<int> my = new MyList<int>();
+            my.Add(x);
+            Console.WriteLine(my[1]);
+            Console.ReadKey();
+        }
+    }
+}
