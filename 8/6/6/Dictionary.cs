29d9ae4897e6f87cb12cc7550cd4ad99dc222195
olgseey <matyushchenkk@outlook.com>
2018-05-02 22:19:12 +0300

    added 6th task

---
+++ 8/6/6/Dictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    class Dictionary<TKey, TValue>
+    {
+        private struct Node
+        {
+            public TKey tkey;
+            public TValue tvalue;
+        }
+        List<Node> Nodes = new List<Node>();
+        public void Add(TKey key, TValue value)
+        {
+            Node node = new Node
+            {
+                tkey = key,
+                tvalue = value
+            };
+            Nodes.Add(node);
+            count++;
+        }
+        public TValue this[int index]
+        {
+            get
+            {
+                return Nodes[index].tvalue;
+            }
+        }
+        private int count;
+
+        public int Count { get => count;}
+    }
+}
---
+++ 8/6/6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> mydictionary = new Dictionary<int, string>();
+            int n;
+            bool flag = true;
+            while (flag == true)
+            {
+                n = int.Parse(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter Number");
+                            int number = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter Text");
+                            string text = Console.ReadLine();
+                            mydictionary.Add(number, text);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Enter Index");
+                            int x = int.Parse(Console.ReadLine());
+                            Console.WriteLine(mydictionary[x]);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Emount of Numbers is: " + mydictionary.Count);
+                            break;
+                        }
+                    default:
+                        {
+                            flag = false;
+                            break;
+                        }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
