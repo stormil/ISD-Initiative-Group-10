61584e3ac5e6ca111d646d36c0d3c5b39275bc7e
stormil <vasyelen@mail.ru>
2018-05-03 21:39:41 +0300

    8.7 small fixes

--- 8/7/7/Program.cs
+++ 8/7/7/Program.cs
@@ -12,24 +12,23 @@ namespace _7
         static void Main(string[] args)
         {
             ArrayList list = new ArrayList();
-            list.Add(1, "demonstration");
+            list.Add(1, "demonstration of work");
             list[0] = (int)list[0] + 1;
+
             for (int i = 0; i < list.Count; i++)
             {
                 Console.WriteLine(list[i].ToString());
             }
-            list.Clear();
 
+            list.Clear();
             list.Add("stranger", "dog", "apple", 34, 34.34);
             list.Remove("stranger");
             list.RemoveAt(0);
+
             if (list.Contains("apple"))
             {
                 Console.WriteLine("List contains \"apple\"" + " at index # " + list.IndexOf("apple"));
             }
-
-            
-
 
             Console.ReadKey();
         }
