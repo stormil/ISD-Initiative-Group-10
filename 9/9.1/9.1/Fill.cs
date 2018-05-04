6fe22eed57a4404c730a619dd5ce4443d47379e1
BorysSudos <boruha18@gmail.com>
2018-05-04 23:33:05 +0300

    Borys 9.1 done

---
+++ 9/9.1/9.1/Fill.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1
+{
+    class Fill
+    {
+        static int[] array = new int[10];
+        public Fill()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        List<int> collection = new List<int> (array);
+         
+        public static IEnumerable<int> Filter()
+        {
+            for (int i; i<array.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
---
+++ 9/9.1/9.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fill fill = new Fill();
+            fill.
+
+
+            Console.ReadKey();
+        }
+    }
+}
