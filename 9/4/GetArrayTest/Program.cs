2ed21b75928d6c34d933b7e83d28b7dbb10762a5
IvanCodeC <otb.tatotake@gmail.com>
2018-05-04 18:30:50 +0300

    4th is done

---
+++ 9/4/GetArrayTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetArrayTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>
+            {
+                1,2,3,4,5,6,7,8,9,11
+            };
+
+            int[] arr = list.GetArray();
+
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 9/4/GetArrayTest/Util.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetArrayTest
+{
+    static class Util
+    {
+        public static T[] GetArray<T>(this IEnumerable<T> list)
+        {
+            IEnumerator<T> l = list.GetEnumerator();
+            l.Reset();
+
+            int size = 0; 
+
+            while(l.MoveNext())
+            {
+                size++;
+            }
+            l.Reset();
+
+            T[] array = new T[size];
+
+            l.MoveNext();
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = l.Current;
+                l.MoveNext();
+            }
+    
+            return array;
+        }
+    }
+}
