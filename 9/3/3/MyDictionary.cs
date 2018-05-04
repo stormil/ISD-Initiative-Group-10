2677b1003c46a49a9cf8f3b197c022954c62b766
stormil <vasyelen@mail.ru>
2018-05-04 22:27:44 +0300

    9.3 done

---
+++ 9/3/3/MyDictionary.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Pair<TKey, TValue>
+    {
+        public Pair(TKey key, TValue value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+
+        public TValue Value { get; set; }
+        public TKey Key { get; set; }
+
+        public override string ToString()
+        {
+            return "[ " + Key + ", " + Value + " ]";
+        }
+    }
+
+    class MyDictionary<TKey, TValue> : IEnumerable <Pair<TKey, TValue>>
+    {
+        List<TKey> keys = new List<TKey>();
+        List<TValue> values = new List<TValue>();
+        public int Amount { get => keys.Count; }
+        public List<TKey> Keys
+        {
+            get
+            {
+                return keys;
+            }
+        }
+        public List<TValue> Values
+        {
+            get
+            {
+                return values;
+            }
+        }
+        public TValue this[TKey index]
+        {
+            get
+            {
+                if (keys.Contains(index))
+                {
+                    return values[keys.IndexOf(index)];
+                }
+                throw new ArgumentException();
+            }
+            set => Add(index, value);
+        }
+        public void Add(TKey key, TValue value)
+        {
+            if (keys.Contains(key))
+            {
+                values[keys.IndexOf(key)] = value;
+            }
+            else
+            {
+                keys.Add(key);
+                values.Add(value);
+            }
+        }
+
+        public IEnumerator<Pair<TKey, TValue>> GetEnumerator()
+        {
+            return new MyDictionaryEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        class MyDictionaryEnumerator: IEnumerator<Pair<TKey, TValue>>
+        {
+            MyDictionary<TKey, TValue> dictionary;
+            TKey currentKey;
+            TValue currentValue;
+            int index;
+
+            public MyDictionaryEnumerator(MyDictionary<TKey, TValue> dictionary)
+            {
+                this.dictionary = dictionary;
+                currentKey = default(TKey);
+            }
+
+            public Pair<TKey, TValue> Current
+            {
+                get
+                {
+                    currentKey = dictionary.Keys[index];
+                    currentValue = dictionary.Values[index];
+                    return new Pair<TKey, TValue>(currentKey, currentValue);
+                }
+            }
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                if (EqualityComparer<TKey>.Default.Equals(currentKey, default(TKey)))
+                {
+                    index = 0;
+                }
+                else
+                {
+                    index++;
+                }
+                return index < dictionary.Keys.Count;
+            }
+
+            public void Reset()
+            {
+            }
+        }
+    }
+}
---
+++ 9/3/3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string> // my dictionary
+            {
+                { 1, "house" },
+                { 2, "dog" },
+                { 3, "Leo" },
+                { 342, "day" }
+            };
+
+            Dictionary<int, string> standartDictionary = new Dictionary<int, string> // standart dictionary
+            {
+                { 5, "home" },
+                { 3, "cat" },
+                { 4, "Kate" },
+                { 334, "night" }
+            };
+            Console.WriteLine("Foreach in standart dictionary collection:");
+            foreach (var entry in standartDictionary)
+            {
+                Console.WriteLine(entry);
+            }
+
+            Console.WriteLine("\nForeach in my dictionary collection:");
+            foreach (var entry in myDictionary)
+            {
+                Console.WriteLine(entry);
+            }
+
+            Console.WriteLine("\nForeach just for values in my dictionary:");
+            foreach (var entry in myDictionary.Values)
+            {
+                Console.WriteLine(entry);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
