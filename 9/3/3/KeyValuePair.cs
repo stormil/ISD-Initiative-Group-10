0f7b286fdf54be010be81123b200e60324371ae0
stormil <vasyelen@mail.ru>
2018-05-05 12:19:39 +0300

    9.3 refactoring to Coding Standarts

---
+++ 9/3/3/KeyValuePair.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class KeyValuePair<TKey, TValue>
+    {
+        public KeyValuePair(TKey key, TValue value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+
+        public TValue Value { get; set; }
+        public TKey Key { get; set; }
+
+        public override string ToString()
+        {
+            string result = "[";
+            if (Key != null) result += Key.ToString();
+            result += ", ";
+            if (Value != null)
+            {
+                result += Value.ToString();
+            }
+            result += "]";
+            return result;
+        }
+    }
+}
--- 9/3/3/MyDictionary.cs
+++ 9/3/3/MyDictionary.cs
@@ -7,49 +7,20 @@ using System.Threading.Tasks;
 
 namespace _3
 {
-    class KeyValuePair<TKey, TValue>
-    {
-        public KeyValuePair(TKey key, TValue value)
-        {
-            this.Key = key;
-            this.Value = value;
-        }
-
-        public TValue Value { get; set; }
-        public TKey Key { get; set; }
-
-        public override string ToString()
-        {
-            return "[" + Key + ", " + Value + "]";
-        }
-    }
-
     class MyDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
     {
-        List<TKey> keys = new List<TKey>();
-        List<TValue> values = new List<TValue>();
-        public int Amount { get => keys.Count; }
-        public List<TKey> Keys
-        {
-            get
-            {
-                return keys;
-            }
-        }
-        public List<TValue> Values
-        {
-            get
-            {
-                return values;
-            }
-        }
+        public int Amount { get => Keys.Count; }
+        public List<TKey> Keys { get; } = new List<TKey>();
+
+        public List<TValue> Values { get; } = new List<TValue>();
+
         public TValue this[TKey index]
         {
             get
             {
-                if (keys.Contains(index))
+                if (Keys.Contains(index))
                 {
-                    return values[keys.IndexOf(index)];
+                    return Values[Keys.IndexOf(index)];
                 }
                 throw new ArgumentException();
             }
@@ -57,22 +28,22 @@ namespace _3
         }
         public void Add(TKey key, TValue value)
         {
-            if (keys.Contains(key))
+            if (Keys.Contains(key))
             {
-                values[keys.IndexOf(key)] = value;
+                Values[Keys.IndexOf(key)] = value;
             }
             else
             {
-                keys.Add(key);
-                values.Add(value);
+                Keys.Add(key);
+                Values.Add(value);
             }
         }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
-            for (int i = 0; i < keys.Count; i++)
+            for (int i = 0; i < Keys.Count; i++)
             {
-                yield return new KeyValuePair<TKey, TValue>(keys[i], values[i]);
+                yield return new KeyValuePair<TKey, TValue>(Keys[i], Values[i]);
             }
         }
 
@@ -83,11 +54,11 @@ namespace _3
 
         public bool ContainsKey(TKey key)
         {
-            return keys.Contains(key);
+            return Keys.Contains(key);
         }
         public bool ContainsValue(TValue value)
         {
-            return values.Contains(value);
+            return Values.Contains(value);
         }
     }
 }
