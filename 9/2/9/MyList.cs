4e36e8efce12338c6cd64271f68377f3cf3b79ec
olgseey <matyushchenkk@outlook.com>
2018-05-05 23:31:10 +0300

    commit task

---
+++ 9/2/9/MyList.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+
+    class MyList<T> : IEnumerable<T>
+    {
+        private class Node
+        {
+            public T info;
+            public Node next;
+            public int index;
+        }
+        private Node begin;
+        private int current;
+        public void Add(T value)
+        {
+            Node temp = new Node()
+            {
+                info = value
+            };
+            temp.next = begin;
+            begin = temp;
+            temp.index = count;
+            count++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = begin;
+            while (current != null)
+            {
+                yield return current.info;
+                current = current.next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public MyList()
+        {
+            Node temp = new Node
+            {
+                next = begin
+            };
+            begin = null;
+            count = 0;
+            current = -1;
+        }
+        private int count;
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                Node temp = begin;
+                while (temp != null)
+                {
+                    if (temp.index == index)
+                    {
+                        break;
+                    }
+                }
+                return temp.info;
+            }
+        }
+       
+    }
+}
---
+++ 9/2/9/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                MyList<int> my = new MyList<int>();
+                int n;
+                bool flag = true;
+                while (flag == true)
+                {
+                    n = int.Parse(Console.ReadLine());
+                    switch (n)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Enter Number");
+                                int x = int.Parse(Console.ReadLine());
+                                my.Add(x);
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("Enter Index");
+                                int x = int.Parse(Console.ReadLine());
+                                Console.WriteLine(my[x]);
+                                break;
+                            }
+                        case 3:
+                            {
+                                Console.WriteLine("Emount of Numbers is: " + my.Count);
+                                break;
+                            }
+                        case 4:
+                            {
+                                Console.WriteLine("Inputed Elements: ");
+                                foreach (int element in my)
+                                {
+                                    Console.WriteLine(element);
+                                }
+                                Console.ReadKey();
+                                break;
+                            }
+                        default:
+                            {
+                                flag = false;
+                                break;
+                            }
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
