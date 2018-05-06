0e7ba5d47a7eda4142c9f66cce5e5553b7ace50a
olgseey <matyushchenkk@outlook.com>
2018-05-06 13:38:51 +0300

    added 2nd task

---
+++ 10/2/2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            AddNameOfFile(out path);
+            Console.WriteLine(path);
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            using (StreamWriter myWriter = new StreamWriter(path))
+            {
+                Console.WriteLine("Input your text:");
+                string text = Console.ReadLine();
+                myWriter.WriteLine(text);
+            }
+
+            using (StreamReader myReader = new StreamReader(path))
+            {
+                Console.WriteLine("Inputed Text:");
+                Console.Write(myReader.ReadToEnd());
+            }
+            Console.ReadKey();
+        }
+        static void AddNameOfFile(out string path)
+        {
+            path = @"D:\";
+            Console.WriteLine("Input name of file: ");
+            string temp = Console.ReadLine(); 
+            path += temp + ".txt";
+        }
+    }
+
+}
